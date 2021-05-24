f7e0efd1fac7d9af1a2aab9c2087479ae474e4ac
Jasir K <jasir.k@factset.com>
2021-05-24 21:21:50 +0530

    starting

---
+++ src/App.vue
@@ -0,0 +1,78 @@
+<template>
+  <main>
+    <user-list :users="activeUsers" @list-projects="selectUser"></user-list>
+    <projects-list :user="selectedUser"></projects-list>
+    <form @submit.prevent="submit">
+    <rating v-model="rating"></rating>
+    <button></button>
+      </form>
+  </main>
+</template>
+
+<script>
+import USER_DATA from './dummy-data.js';
+
+import UserList from './components/users/UserList.vue';
+import ProjectsList from './components/projects/ProjectsList.vue';
+import Rating from './components/form/Rating.vue';
+
+export default {
+  components: {
+    UserList,
+    ProjectsList,
+    Rating
+  },
+  data() {
+    return {
+      selectedUser: null,
+      activeUsers: USER_DATA,
+      rating: null
+    };
+  },
+  methods: {
+    selectUser(uid) {
+      this.selectedUser = this.activeUsers.find((usr) => usr.id === uid);
+    },
+    submit() {
+      console.log('called');
+      console.log(this.rating);
+    }
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+html {
+  font-family: sans-serif;
+}
+body {
+  margin: 0;
+}
+
+main {
+  display: flex;
+  justify-content: space-around;
+}
+
+button {
+  font: inherit;
+  border: 1px solid #00006b;
+  background-color: transparent;
+  color: #00006b;
+  padding: 0.5rem 1.5rem;
+  cursor: pointer;
+  margin: 0.5rem 0.5rem 0.5rem 0;
+}
+button:hover,
+button:active {
+  background-color: #efefff;
+}
+
+button.selected {
+  background-color: #00006b;
+  color: white;
+}
+</style>
---
+++ src/components/form/Rating.vue
@@ -0,0 +1,31 @@
+<template>
+    <ul>
+        <li><button :class="{active: selected === 'one'}" @click="select('one')" type="button">One</button></li>
+        <li><button :class="{active: selected === 'two'}" @click="select('two')" type="button">Two</button></li>
+    </ul>
+</template>
+
+<script>
+export default {
+    props: ['ratingValue'],
+    emits: ['update:ratingValue'],
+    data() {
+        return {
+            selected: this.ratingValue
+        }
+    },
+    methods: {
+        select(value) {
+            this.selected = value;
+            this.$emit('update:ratingValue', value);
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .active {
+        background-color:red;
+    }
+</style>
